cfb8af2c51815af4f52254f94108d3c2f6566815
Jeremy Cole <jeremy@jcole.us>
2019-04-13 23:59:00 -0700

    Add tests for push, pop

---
+++ spec/avr/opcode/data/pop_spec.rb
@@ -0,0 +1,15 @@
+require 'shared_examples_for_opcode'
+
+RSpec.describe [AVR::Opcode, :push] do
+  include_examples "opcode", :push
+
+  it "pops the the stack into a register" do
+    @cpu.sram.memory[@cpu.sp.value].value = 1
+    @cpu.sp.value -= 1
+    previous_sp = @cpu.sp.value
+    @cpu.instruction(0, :pop, @cpu.r0).execute
+
+    expect(@cpu.r0.value).to be 1
+    expect(@cpu.sp.value).to eq previous_sp + 1
+  end
+end
---
+++ spec/avr/opcode/data/push_spec.rb
@@ -0,0 +1,15 @@
+require 'shared_examples_for_opcode'
+
+RSpec.describe [AVR::Opcode, :push] do
+  include_examples "opcode", :push
+
+  it "pushes the register onto the stack" do
+    @cpu.r0 = 1
+    previous_sp = @cpu.sp.value
+    @cpu.instruction(0, :push, @cpu.r0).execute
+
+    expect(@cpu.r0.value).to be 1
+    expect(@cpu.sram.memory[@cpu.sp.value+1].value).to eq 1
+    expect(@cpu.sp.value).to eq previous_sp - 1
+  end
+end
