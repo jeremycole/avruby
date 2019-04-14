df542dafd89764765a52ecdd3c9244405c89194f
Jeremy Cole <jeremy@jcole.us>
2019-04-13 20:12:16 -0700

    Implement bitwise opcodes

--- lib/avr/opcode/math/bitwise.rb
+++ lib/avr/opcode/math/bitwise.rb
@@ -1,13 +1,43 @@
 module AVR
   class Opcode
+    def self.set_sreg_for_and_or(cpu, r, rd, rr)
+      r7  = (r & (1<<7)) != 0
+
+      cpu.sreg.set_by_hash({
+        S: r7 ^ false,
+        V: false,
+        N: r7,
+        Z: (r == 0),
+      })
+    end
+
+    opcode(:and, [:register, :register]) do |cpu, memory, offset, args|
+      result = (args[0].value & args[1].value)
+      set_sreg_for_and_or(cpu, result, args[0].value, args[1].value)
+      args[0].value = result
+    end
+
+    opcode(:andi, [:register, :byte]) do |cpu, memory, offset, args|
+      result = (args[0].value & args[1])
+      set_sreg_for_and_or(cpu, result, args[0].value, args[1])
+      args[0].value = result
+    end
+
     opcode(:eor, [:register, :register]) do |cpu, memory, offset, args|
       result = (args[0].value ^ args[1].value)
-      cpu.sreg.set_by_hash({
-        S: false, # TODO
-        V: false,
-        N: ((result & 0x80) == 0x80),
-        Z: (result == 0),
-      })
+      set_sreg_for_and_or(cpu, result, args[0].value, args[1].value)
+      args[0].value = result
+    end
+
+    opcode(:or, [:register, :register]) do |cpu, memory, offset, args|
+      result = (args[0].value | args[1].value)
+      set_sreg_for_and_or(cpu, result, args[0].value, args[1].value)
+      args[0].value = result
+    end
+
+    opcode(:ori, [:register, :byte]) do |cpu, memory, offset, args|
+      result = (args[0].value | args[1].value)
+      set_sreg_for_and_or(cpu, result, args[0].value, args[1])
       args[0].value = result
     end
   end
