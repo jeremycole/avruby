4d28b722e057e343a1dc3211c4d380f5c2e05a24
Jeremy Cole <jeremy@jcole.us>
2019-04-13 23:35:02 -0700

    Add tests for brbs brbc

---
+++ spec/avr/opcode/branch/brbs_spec.rb
@@ -0,0 +1,21 @@
+require 'shared_examples_for_opcode'
+
+RSpec.describe AVR::Opcode, :brbs do
+  include_examples "opcode", :brbs
+
+  after(:each) do
+    @cpu.sreg.reset
+  end
+
+  it "branches if the bit is set" do
+    @cpu.sreg.Z = true
+    @cpu.instruction(0, :brbs, :Z, +20).execute
+    expect(@cpu.pc).to eq 21
+  end
+
+  it "does not branch if the bit is clear" do
+    @cpu.sreg.Z = false
+    @cpu.instruction(0, :brbs, :Z, +20).execute
+    expect(@cpu.pc).to eq 1
+  end
+end
