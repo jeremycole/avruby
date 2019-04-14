5d7e0d8bbdbd8ad75d785e542a630950dfaba914
Jeremy Cole <jeremy@jcole.us>
2019-04-13 22:28:11 -0700

    Small refactor of mov test

--- spec/avr/opcode/mov_spec.rb
+++ spec/avr/opcode/mov_spec.rb
@@ -3,17 +3,19 @@ require 'shared_examples_for_opcode'
 RSpec.describe AVR::Opcode, :mov do
   include_examples "opcode", :mov
 
-  it "copies the source register to the target register" do
-    i = @cpu.instruction(0, :mov, @cpu.r0, @cpu.r1)
+  before(:all) do
+    @i = @cpu.instruction(0, :mov, @cpu.r0, @cpu.r1)
+  end
 
+  it "copies the source register to the target register" do
     @cpu.r0 = 0
     @cpu.r1 = 1
-    i.execute
+    @i.execute
     expect(@cpu.r0.value).to be 1
     expect(@cpu.r1.value).to be 1
     @cpu.r0 = 1
     @cpu.r1 = 0
-    i.execute
+    @i.execute
     expect(@cpu.r0.value).to be 0
     expect(@cpu.r1.value).to be 0
   end
